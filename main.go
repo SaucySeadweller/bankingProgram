7ed5e1e16d9eecbe41dd2e99e002ce74196fa543
Chase <joli.chase@gmail.com>
2019-05-22 19:15:42 -0700

    added a little more to the package

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package bank
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
@@ -13,17 +16,59 @@ type User struct {
 	Password string
 	Pin      int
 	Balance  int
+	Commands []Command
+}
+
+//Command is struct containing actions a user can take
+type Command struct {
+	checkBalance string
+	deposit      string
+	withdraw     string
+}
+
+func main() {
+	fmt.Println(" Please login or register")
+
 }
 
 func (u *User) register(email string, password string, pin int) {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println(scanner)
+
 }
 
-func (u *User) transact() {
+func (u *User) transact(command string) (int int) {
+	balance := u.Balance
 
+	return balance
 }
 
 func (u *User) login(email string, password string, pin int) {
+	scanner := bufio.NewScanner(os.Stdin)
 
+	fmt.Println(scanner)
+}
+
+//Save saves a user's account information
+func (u *User) Save(user string) {
+	u.Email = user
+	data, err := json.Marshal(u)
+	if err != nil {
+		log.Fatalln("Failed to save")
+	}
+	err = ioutil.WriteFile(user, data, 0644)
+	if err != nil {
+		log.Fatalln("Failed to write.")
+	}
+}
+
+//Load loads a user's account information
+func (u *User) Load(user string) {
+	dat, err := ioutil.ReadFile(user)
+
+	err = json.Unmarshal(dat, u)
+	if err != nil {
+		log.Println("no data file")
+	}
 }
