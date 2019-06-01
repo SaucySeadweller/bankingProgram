11da2e2b32a5acf1f58c1f535a60bcda7b1d79c4
Chase <joli.chase@gmail.com>
2019-05-31 21:15:59 -0700

    add transaction log, adjusted login values

--- bank/bank.go
+++ bank/bank.go
@@ -11,7 +11,6 @@ import (
 //User is a struct that defines what a fields a User has
 type User struct {
 	Name         string
-	Email        string
 	Password     string
 	Balance      float64
 	Transactions []Transaction
@@ -19,14 +18,15 @@ type User struct {
 
 //Transaction is a struct that contains the amount and time of a transaction
 type Transaction struct {
-	amount float64
-	date   time.Time
+	Amount float64
+	Date   time.Time
 }
 
 //Register registers a new user in the system
-func Register(email string, password string) *User {
+func Register(Name string, password string) *User {
+
 	return &User{
-		Email:    email,
+		Name:     Name,
 		Password: password,
 	}
 }
@@ -38,38 +38,44 @@ func (u *User) accountBalance() float64 {
 //Deposit deposits an amount into the user's account
 func (u *User) Deposit(amount float64) float64 {
 	u.NewTransaction(amount, time.Now())
-	u.Balance -= amount
+	u.Balance += amount
+
 	return u.Balance
 }
 
 //Withdrawl withdraws an amount from the user's account
-func (u *User) Withdrawl(amount float64) float64 {
+func (u *User) Withdrawl(amount float64) (int, float64) {
 	u.NewTransaction(amount, time.Now())
-	u.Balance += amount
-	return u.Balance
+	u.Balance -= amount
+	return 0, u.Balance
+}
+
+//ViewTransactions prints out prior tansactions and their time stamps
+func (u *User) ViewTransactions() string {
+	str := ""
+	for _, t := range u.Transactions {
+		str += fmt.Sprintf("%f %s \n", t.Amount, t.Date.Format(time.ANSIC))
+	}
+	return str
 }
 
 //Login allows the user to login to their account
-func Login(email string, password string) (*User, error) {
+func Login(Name string, password string) (*User, error) {
 	u := &User{}
-	if !u.Load(email) {
-		return nil, fmt.Errorf("invalid email/password")
+	if !u.Load(Name) {
+		return nil, fmt.Errorf("invalid Name/password")
 	}
-	if u.Password != password || u.Email != email {
-		return nil, fmt.Errorf("incorrect email/password")
+	if u.Password != password || u.Name != Name {
+		return nil, fmt.Errorf("incorrect Name/password")
 	}
 	return u, nil
-}
-
-//TransactionLog brings up a log of the users past transactions
-func (u *User) TransactionLog() {
-	fmt.Println(u.Transactions)
 }
 
 //NewTransaction keeps track of the transactions a user makes
 func (u *User) NewTransaction(amount float64, when time.Time) {
 	u.Transactions = append(u.Transactions, Transaction{amount, when})
 	fmt.Println("transaction made:", u.Transactions[len(u.Transactions)-1])
+
 }
 
 //Save saves a user's account information
@@ -78,7 +84,7 @@ func (u *User) Save() {
 	if err != nil {
 		log.Fatalln("Failed to save")
 	}
-	err = ioutil.WriteFile(u.Email, data, 0644)
+	err = ioutil.WriteFile(u.Name, data, 0644)
 	if err != nil {
 		log.Fatalln("Failed to write.")
 	}
