1e1dc918fcbd459e0affcf5220895e0e308066ea
Chase <joli.chase@gmail.com>
2019-05-31 17:17:31 -0700

    now welomes a user with their name upon logging in, changed email credentials to name.

--- main.go
+++ main.go
@@ -9,35 +9,34 @@ import (
 )
 
 func main() {
-
 	fmt.Println("Please login or register by typing login or register")
 	cmd := GetString()
 	executeCmd(cmd)
 }
-
 func executeCmd(cmd string) {
 
 	switch strings.ToLower(cmd) {
 	default:
-
 		log.Println("command not found", cmd)
-
 	case "register":
-		u := bank.Register(GetEmailPass())
+		u := bank.Register(GetNamePass())
 		log.Println(u)
+		fmt.Println("Please make an intial deposit.")
+		u.Deposit(GetFloat())
+		fmt.Println(u.Balance)
 		u.Save()
 
 	case "login":
-		u, err := bank.Login(GetEmailPass())
+		u, err := bank.Login(GetNamePass())
 		if err != nil {
 			log.Fatal(err)
-
 		}
-		fmt.Println("Welcome: ", u.Name)
+
+		fmt.Println("Welcome:", u.Name)
+
 		for {
 			fmt.Println("What do would you like to do?")
 			cmd := GetString()
-
 			switch strings.ToLower(cmd) {
 			default:
 				log.Println("command not found", cmd)
@@ -45,7 +44,6 @@ func executeCmd(cmd string) {
 				fmt.Println(u.ViewTransactions())
 			case "balance":
 				fmt.Println(u.Balance)
-
 			case "deposit":
 				fmt.Println("enter an amount to deposit")
 				u.Deposit(GetFloat())
@@ -60,16 +58,15 @@ func executeCmd(cmd string) {
 	}
 }
 
-//GetEmailPass checks login credentials
-func GetEmailPass() (string, string) {
-	var email string
+//GetNamePass checks login credentials
+func GetNamePass() (string, string) {
+	var name string
 	var password string
-	fmt.Println("What is your email?")
-	fmt.Scanf("%s", &email)
+	fmt.Println("What is your name?")
+	fmt.Scanf("%s", &name)
 	fmt.Println("What is your password?")
 	fmt.Scanf("%s", &password)
-	return email, password
-
+	return name, password
 }
 
 //GetString gets commands from the user
